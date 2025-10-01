1615b1688de8bbc616331f7df741636fa302d767
don-munnex <muniradeiza01@gmail.com>
2025-10-01 19:51:05 +0100

    jwt mod

--- src/components/fetchCurrentUser.ts
+++ src/components/fetchCurrentUser.ts
@@ -1,6 +1,6 @@
 export async function fetchCurrentUser() {
-  const VITE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
-  const resp = await fetch(`${VITE_API_URL}/Itiza_Delivery/auth/me`, {
+  // const VITE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+  const resp = await fetch(`https://itiza-backend.vercel.app/api/auth/me`, {
     method: "GET",
     credentials: "include", // send cookie
   });
